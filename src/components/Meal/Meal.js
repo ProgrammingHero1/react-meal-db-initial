925c0c2d055cf1e77192b9c9cb49149ba6f5ea96
Programming Hero <programming.hero1@gmail.com>
2022-03-24 22:06:08 -0500

    react meal db initial

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -3,13 +3,13 @@ import './Meal.css';
 
 const Meal = (props) => {
     const { strMeal, strInstructions, strMealThumb } = props.meal;
-    const { handleAddToOrder, meal } = props;
+    
     return (
         <div className="meal">
             <img src={strMealThumb} alt="" />
             <h4>{strMeal}</h4>
             <p>{strInstructions.slice(0, 100)}</p>
-            <button onClick={() => handleAddToOrder(meal)}>Add this Food</button>
+            <button>Add this Food</button>
         </div>
     );
 };
--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb, getDb } from '../../localstorage/localstorage';
 import Meal from '../Meal/Meal';
 import OrderList from '../OrderList/OrderList';
 import './Restaurant.css';
 
 const Restaurant = () => {
     const [meals, setMeals] = useState([]);
-    const [order, setOrder] = useState([]);
+    const [orders, setOrders] = useState([]);
 
     useEffect(() => {
         fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
@@ -27,26 +26,7 @@ const Restaurant = () => {
         Read carefully, give it a try. [ Ki ache jibone]
         if  you need help, let us know in the support session
     */
-    useEffect(() => {
-        if (meals.length) {
-            const savedDb = getDb();
-            const savedOrder = [];
-            for (const mealId in savedDb) {
-                const meal = meals.find(ml => ml.idMeal === mealId)
-                const quantity = savedDb[mealId];
-                meal.quantity = quantity;
-                savedOrder.push(meal);
-            }
-            setOrder(savedOrder);
-        }
-    }, [meals])
-
-    const handleAddToOrder = meal => {
-        meal.quantity = 1;
-        const newOrder = [...order, meal];
-        setOrder(newOrder);
-        addToDb(meal.idMeal);
-    }
+    
 
     return (
         <div className="restaurant-menu">
@@ -55,12 +35,11 @@ const Restaurant = () => {
                     meals.map(meal => <Meal
                         key={meal.idMeal}
                         meal={meal}
-                        handleAddToOrder={handleAddToOrder}
                     ></Meal>)
                 }
             </div>
             <div className="order-list">
-                <OrderList order={order}></OrderList>
+                <OrderList orders={orders}></OrderList>
             </div>
         </div>
 
